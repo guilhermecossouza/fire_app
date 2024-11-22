b795c52b29bb0fbd2a0435488b0d725736755b43
guilhermecossouza@gmail.com <guilhermecossouza@gmail.com>
2024-11-22 17:59:40 -0300

    termino crud com firebase

--- fire-app/src/App.js
+++ fire-app/src/App.js
@@ -1,7 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { db } from './firebaseConnection';
 //import { doc, setDoc } from 'firebase/firestore';
-import { doc, collection, addDoc, getDoc, getDocs } from 'firebase/firestore';
+import { 
+  doc, 
+  collection, 
+  addDoc, 
+  getDoc, 
+  getDocs, 
+  updateDoc, 
+  deleteDoc,
+  onSnapshot
+
+} from 'firebase/firestore';
 
 import './app.css';
 
@@ -9,6 +19,25 @@ export default function App() {
   const [titulo, setTitulo] = useState("");
   const [autor, setAutor] = useState("");
   const [posts, setPosts] = useState([]);
+  const [idPost, setIdPost] = useState("");
+
+  useEffect(() => {
+    async function loadPosts() {
+      // await onSnapshot(collection(db, "posts"), (snapshot) => {
+      //   const listaPost = [];
+      //   snapshot.forEach((doc) => {
+      //     listaPost.push({
+      //       id: doc.id,
+      //       titulo: doc.data().titulo,
+      //       autor: doc.data().autor
+      //     });
+      //   });
+      //   setPosts(listaPost);
+      // });
+    }
+
+    loadPosts();
+  });
 
   async function handleAdd() {
     // await setDoc(doc(db, "posts", "12345"), {
@@ -57,40 +86,73 @@ export default function App() {
     }).catch((error) => {
       console.log(`Gerou um erro: ${error}`);
     })
+  }
 
+  async function editarPOst() {
+    await updateDoc(doc(db, "posts", idPost), {
+      titulo: titulo,
+      autor: autor
+    }).then(() => {
+      console.log("Post editado com sucesso!");
+      setIdPost("");
+      setTitulo("");
+      setAutor("");
+    }).catch((error) => {
+      console.log(`Gerou um erro: ${error}`);
+    });
 
+  }
 
+  async function excluirPost(id) {
+    await deleteDoc(doc(db, "posts", id)).then(() => {
+      console.log("Post excluido com sucesso!");
+    }).catch((error) => {
+      console.log(`Gerou um erro: ${error}`);
+    })
   }
 
   return (
     <div className="App">
       <h1>Projeto React com FireBase</h1>
       <div className="container">
+        <label>Id do post</label>
+        <input
+          type="text"
+          placeholder="Digite o id do post"
+          value={idPost}
+          onChange={(event) => { setIdPost(event.target.value) }}
+        />
+        <br />
         <label>Titulo:</label>
-        <textarea 
-          type="text" 
+        <textarea
+          type="text"
           placeholder="Digite o título"
-          value={ titulo }
-          onChange={ (event) => { setTitulo(event.target.value) } }
+          value={titulo}
+          onChange={(event) => { setTitulo(event.target.value) }}
         />
 
         <label>Autor:</label>
-        <input 
+        <input
           type="text"
-          placeholder="Digite o autor do post" 
-          value={ autor }
-          onChange={ (event) => { setAutor(event.target.value) }}
+          placeholder="Digite o autor do post"
+          value={autor}
+          onChange={(event) => { setAutor(event.target.value) }}
         />
-
-        <button 
-          type='button' 
-          onClick={ handleAdd }
+        <br />
+        <button
+          type='button'
+          onClick={handleAdd}
         >Cadastrar</button>
-
-        <button 
-          type='button' 
-          onClick={ buscaPost }
+        <br />
+        <button
+          type='button'
+          onClick={buscaPost}
         >Buscar post</button>
+        <br />
+        <button
+          type='button'
+          onClick={editarPOst}
+        >editar post</button>
 
         <ul>
           {posts.map((post) => {
@@ -98,7 +160,12 @@ export default function App() {
               <li key={post.id}>
                 <strong>Título:</strong> {post.id} <br />
                 <strong>Título:</strong> {post.titulo} <br />
-                <strong>Autor:</strong> {post.autor}
+                <strong>Autor:</strong> {post.autor} <br />
+                <button
+                  type='button'
+                  onClick={() => { excluirPost(post.id) }}
+                >Deleta post</button> <br /><br />
+
               </li>
             );
           })}
